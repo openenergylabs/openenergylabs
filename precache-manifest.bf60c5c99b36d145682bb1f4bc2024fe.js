f57108137a78554bc5f5e7699118565fa3dfc912
abednegotm <abednegomwanza@gmail.com>
2020-06-03 23:07:48 +0200

    Updates

---
+++ precache-manifest.bf60c5c99b36d145682bb1f4bc2024fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bceec79af49bcb00705ce447a3c77469",
+    "url": "/openenergylabs/index.html"
+  },
+  {
+    "revision": "15f0263d1d611249d34d",
+    "url": "/openenergylabs/static/css/main.321f4397.chunk.css"
+  },
+  {
+    "revision": "e71c821cca0edd47e322",
+    "url": "/openenergylabs/static/js/2.56f878d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/openenergylabs/static/js/2.56f878d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f0263d1d611249d34d",
+    "url": "/openenergylabs/static/js/main.1e32de7c.chunk.js"
+  },
+  {
+    "revision": "dfbb321c44916e31219e",
+    "url": "/openenergylabs/static/js/runtime-main.1738e495.js"
+  },
+  {
+    "revision": "079052da4c90a5561ac2ffe818012b66",
+    "url": "/openenergylabs/static/media/Capture.079052da.png"
+  },
+  {
+    "revision": "5b079e57406179f8d27106ddde2d9cb7",
+    "url": "/openenergylabs/static/media/Down_by_the_River.5b079e57.mp4"
+  },
+  {
+    "revision": "a032f73fe01bbc49c8fc8ebdd4671b85",
+    "url": "/openenergylabs/static/media/Energy.a032f73f.png"
+  },
+  {
+    "revision": "0cc77962739bf61d67513681c58b4e19",
+    "url": "/openenergylabs/static/media/Gellix-Regular.0cc77962.ttf"
+  },
+  {
+    "revision": "2f15a9f37e3235a0bd5aa54135243f61",
+    "url": "/openenergylabs/static/media/Gellix-Regular.2f15a9f3.woff2"
+  },
+  {
+    "revision": "6645d1b8877432cc38934b47dc7b4f03",
+    "url": "/openenergylabs/static/media/Gellix-Regular.6645d1b8.woff"
+  },
+  {
+    "revision": "c4143c829a3ab56770de0b39c343aed9",
+    "url": "/openenergylabs/static/media/Le1.c4143c82.png"
+  },
+  {
+    "revision": "9417448617fe40cd8beaabc092773b6d",
+    "url": "/openenergylabs/static/media/Learning.94174486.png"
+  },
+  {
+    "revision": "43e27845779ccb66c50d19dd98a786bc",
+    "url": "/openenergylabs/static/media/bcv.43e27845.png"
+  },
+  {
+    "revision": "219c693fe1b0471c0d329d8a74db0ccc",
+    "url": "/openenergylabs/static/media/eps.219c693f.png"
+  },
+  {
+    "revision": "5cfeecdb437e345fa7b05f03e06556f6",
+    "url": "/openenergylabs/static/media/fik.5cfeecdb.png"
+  },
+  {
+    "revision": "d5a946805ef61281720393bf51c9a45a",
+    "url": "/openenergylabs/static/media/map.d5a94680.png"
+  }
+]);
