54c6ec7f397168de38e9f86a36c5f3075326b15f
abednegotm <abednegomwanza@gmail.com>
2020-06-05 16:42:21 +0200

    partition app js into saparate components

---
+++ src/sections/footerSection.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import map from "../media/map.png";
+import MailModal from "../components/modal";
+
+function FooterSection() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+
+  return (
+    <footer
+      // className={"section"}
+      style={{ backgroundColor: "#5D4DC4", width: "100%" }}
+    >
+      <Container fluid>
+        <Row>
+          <Col
+            className="footer-lft"
+            lg={7}
+            md={7}
+            sm
+            style={{ width: "100%", position: "relative", padding: "1%" }}
+          >
+            <MailModal show={show} onHide={handleClose} />
+
+            <div
+              id="ft-links"
+              style={{
+                width: "40%",
+                color: "#fff",
+              }}
+            >
+              <p style={{ marginBottom: "1rem" }}>Useful links</p>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <div id="usef-links">
+                  <a href="https://facebook.com/localelectricity">
+                    {" "}
+                    <p>&#8599; Facebook</p>
+                  </a>
+                  <a href="https://twitter.com/openenergylabs">
+                    {" "}
+                    <p>&#8599; Twitter</p>
+                  </a>
+                  <a href="#">
+                    {" "}
+                    <p>&#8599; LinkedIn</p>
+                  </a>
+                  <a href="#">
+                    {" "}
+                    <p>&#8599; Youtube</p>
+                  </a>
+                </div>
+
+                <div>
+                  <a
+                    style={{ color: "#fff", textDecoration: "underline" }}
+                    href={"mailto:hello@localelectricity.org"}
+                  >
+                    Contact Us{" "}
+                  </a>
+                </div>
+              </div>{" "}
+            </div>
+
+            <div
+              id="ft-copyright"
+              style={{
+                position: "absolute",
+                bottom: "1%",
+                color: "#fff",
+              }}
+            >
+              <p style={{ marginBottom: "1rem" }}>
+                &#169; Copyright {new Date().getFullYear()}, Open Energy Labs
+              </p>
+              <p>32/6 Joseph Mwila Rd, Lusaka Zambia</p>
+            </div>
+          </Col>
+          <Col>
+            <img width={"100%"} height={"100%"} src={map} />
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default FooterSection;
---
+++ src/sections/partners.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import OelButton from "../components/button";
+import Container from "react-bootstrap/Container";
+// partners logos
+import bcv from "../media/bcv.png";
+import eps from "../media/eps.png";
+import fik from "../media/fik.png";
+import uik from "../media/uik.png";
+import Partners from "../components/partners";
+import MailModal from "../components/modal";
+
+function PartnersSection() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <section
+      id="partners"
+      className={"section"}
+      style={{
+        backgroundColor: "#FF7F48",
+        display: "flex",
+        alignItems: "center",
+        // justifyContent: "center",
+      }}
+    >
+      <MailModal show={show} onHide={handleClose} />
+      <Container
+        fluid
+        style={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "3%",
+        }}
+      >
+        <div
+          style={{
+            marginBottom: "2.5em",
+            flexDirection: "column",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <h3
+            className="font-glyph"
+            style={{
+              textAlign: "center",
+              color: "#5D4DC4",
+              fontSize: "64px",
+            }}
+          >
+            Our partners & investors
+          </h3>
+          <p
+            style={{
+              textAlign: "center",
+              color: "#5D4DC4",
+              fontSize: "20px",
+              width: "80%",
+            }}
+          >
+            We're backed by some of the leading organisations in energy,
+            development and impact investing.
+          </p>
+        </div>
+        <Partners logos={[eps, fik, uik, bcv, eps]} />
+        <Partners logos={[eps, uik, fik, bcv, eps]} />
+        <Partners logos={[fik, uik, eps, bcv, eps]} />
+        <div style={{ marginTop: 25 }}>
+          <div
+            className="buttonGrp"
+            style={{ marginTop: "1rem", marginBottom: "1rem" }}
+          >
+            <OelButton
+              path="mailto:hello@localelectricity.org"
+              color="#5D4DC4"
+              name="Partner with us"
+              hideAnt={true}
+            />
+            <OelButton
+              onPressed={handleShow}
+              color={"#5D4DC4"}
+              name={"Register to invest"}
+            />
+          </div>
+          {/* <div
+          style={{
+            borderBottom: "1px #5D4DC4 solid",
+            width: "95%",
+            position: "absolute",
+            bottom: "5%",
+          }}
+        ></div> */}
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default PartnersSection;
