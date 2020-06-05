f7aca155b7546d903cda39955a6db166ead1b6aa
abednegotm <abednegomwanza@gmail.com>
2020-06-06 01:50:56 +0200

    redesign videosection with 2 column layout for

--- src/sections/videosection.js
+++ src/sections/videosection.js
@@ -21,52 +21,79 @@ function VideoSection() {
         alignItems: "center",
         justifyContent: "center",
         position: "relative",
+        padding: "50px",
       }}
     >
       <MailModal show={show} onHide={handleClose} />
-      <Container
+
+      <h2 style={{ color: "#FDE3CA", marginBottom: "50px" }}>
+        We’re raising investment soon on Crowdcube
+      </h2>
+
+      <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-          width: "100%",
-          position: "relative",
-          padding: "5%",
+          display: "grid",
+          gridTemplateColumns: "50% 50%",
+          // backgroundColor: "#ff3",
+          width: "80%",
+          // justifyItems: "center",
         }}
       >
-        <p style={{ alignSelf: "flex-end", color: "#fde3ca" }}>
-          Watch our Crowdcube campaign video
-        </p>
-
-        <Vimeo height={500} video="https://vimeo.com/425909940" />
-
         <div
-          className="buttonGrp"
-          style={{ marginTop: "3rem", marginBottom: "1rem" }}
+          style={{
+            width: "70%",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            color: "#FDE3CA",
+          }}
         >
-          <OelButton
-            path="#whatwedo"
-            color="#fde3ca"
-            name="What we do"
-            hideAnt={true}
-          />
-          <OelButton
-            onPressed={handleShow}
-            color={"#fde3ca"}
-            name={"Register to invest"}
-          />
+          <p style={{ fontSize: "20px" }}>Join us on our journey </p>
+          <p style={{ fontSize: "20px", margin: 20 }}>↓</p>
+          <p style={{ textAlign: "justify" }}>
+            Teaching students in Africa how to build renewable electricity
+            supplies, Open Energy Labs is an EdTech company with a purpose. We
+            are raising investment on crowdcube to develop technology and
+            deliver our programme to 110,000 students in Zambia.
+          </p>
+          <p>
+            {" "}
+            We are already backed by Innovate UK and the Zambian National
+            Science and Technology Council and we are now giving you the chance
+            to join us on our journey, as we support young people in Africa to
+            build the renewable energy systems of the future
+          </p>
+
+          <div style={{ marginTop: "30px" }}>
+            <OelButton
+              onPressed={handleShow}
+              color={"#fde3ca"}
+              name={"Register to invest"}
+            />
+          </div>
         </div>
         <div
           style={{
-            borderBottom: "1px #5D4DC4 solid",
-            width: "95%",
-            position: "absolute",
-            bottom: "5%",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            color: "#FDE3CA",
           }}
-        ></div>
-      </Container>
+        >
+          <p style={{ fontSize: "20px" }}>Watch our campaign video</p>
+          <p style={{ fontSize: "20px", margin: 20 }}>↓</p>
+          <Vimeo width={450} video="https://vimeo.com/425909940" />
+        </div>
+      </div>
+      <div style={{ margin: "60px", marginLeft: "100px" }}>
+        <OelButton
+          path="#whatwedo"
+          color="#fde3ca"
+          name="What we do"
+          hideAnt={true}
+        />
+      </div>
       <div
         style={{
           borderBottom: "1px #fde3ca solid",
