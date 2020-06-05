f149e78bcfcbd6fdd6d206743533d8a6caf57cba
abednegotm <abednegomwanza@gmail.com>
2020-06-06 00:38:51 +0200

    Updates

---
+++ precache-manifest.61726eede4b0d10a35cacbd7d3292153.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c8e270e0e5efde0901ac5f1198b3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3d28ea4245025600f71",
+    "url": "/static/css/main.f976b424.chunk.css"
+  },
+  {
+    "revision": "be4409dadc58aca659f5",
+    "url": "/static/js/2.15c4679a.chunk.js"
+  },
+  {
+    "revision": "1fd0058f21c3ac95d37e657d0f5d87a6",
+    "url": "/static/js/2.15c4679a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d28ea4245025600f71",
+    "url": "/static/js/main.4b69577e.chunk.js"
+  },
+  {
+    "revision": "34cb7856bea0c819b6d1",
+    "url": "/static/js/runtime-main.6c5340c9.js"
+  },
+  {
+    "revision": "079052da4c90a5561ac2ffe818012b66",
+    "url": "/static/media/Capture.d1f468f9.png"
+  },
+  {
+    "revision": "a032f73fe01bbc49c8fc8ebdd4671b85",
+    "url": "/static/media/Energy.2c1b3397.png"
+  },
+  {
+    "revision": "0cc77962739bf61d67513681c58b4e19",
+    "url": "/static/media/Gellix-Regular.0cc77962.ttf"
+  },
+  {
+    "revision": "2f15a9f37e3235a0bd5aa54135243f61",
+    "url": "/static/media/Gellix-Regular.2f15a9f3.woff2"
+  },
+  {
+    "revision": "6645d1b8877432cc38934b47dc7b4f03",
+    "url": "/static/media/Gellix-Regular.6645d1b8.woff"
+  },
+  {
+    "revision": "9417448617fe40cd8beaabc092773b6d",
+    "url": "/static/media/Learning.ddfe3962.png"
+  },
+  {
+    "revision": "43e27845779ccb66c50d19dd98a786bc",
+    "url": "/static/media/bcv.c5e59e71.png"
+  },
+  {
+    "revision": "219c693fe1b0471c0d329d8a74db0ccc",
+    "url": "/static/media/eps.3ed450b0.png"
+  },
+  {
+    "revision": "5cfeecdb437e345fa7b05f03e06556f6",
+    "url": "/static/media/fik.205cedfa.png"
+  },
+  {
+    "revision": "d5a946805ef61281720393bf51c9a45a",
+    "url": "/static/media/map.92766823.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e0443a36380b703db198592b7c4ca45.js"
+  "/precache-manifest.61726eede4b0d10a35cacbd7d3292153.js"
 );
 
 self.addEventListener('message', (event) => {
