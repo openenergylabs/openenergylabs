5fe05eec90651931753d4d481014417877691440
Abednego Mwanza <abednegomwanza@gmail.com>
2020-07-03 15:49:03 +0200

    implement large screen layout of what we do section

--- src/sections/whatwedo.js
+++ src/sections/whatwedo.js
@@ -5,10 +5,10 @@ import Pip from "../media/svgComponents/people";
 import Mch from "../media/svgComponents/machine";
 import CarouselComp from "../components/carasoul";
 import "../styles/whatwedo.css";
+import Partners from "../components/partners";
 
 function WhatWeDoSection() {
-  const [, setShow] = useState(false);
-
+  // const [, setShow] = useState(false);
   return (
     <section
       id="whatwedo"
@@ -28,8 +28,8 @@ function WhatWeDoSection() {
         }}
       >
         <h3 className="section-title">What we do</h3>
-        {/* carousel for mobile */}
-        <CarouselComp className={"services"}>
+
+        <div id="wd-lg-wrapper">
           <div className="service-item">
             <Wd2 />
             <p className="service-title">Energy Makers Academy</p>
@@ -55,8 +55,7 @@ function WhatWeDoSection() {
               <p className="linkText">Find out more</p>
             </a>
           </div>
-
-          <div lg={12} sm className="service-item">
+          <div className="service-item">
             <Pip />
             <p className="service-title">Reduce Energy Poverty</p>
             <p className="wd-down-arrow">↓</p>
@@ -68,7 +67,52 @@ function WhatWeDoSection() {
               <p className="linkText">Find out more</p>
             </a>
           </div>
-        </CarouselComp>
+        </div>
+
+        {/* carousel for mobile */}
+        <div id="wd-sm-wrapper">
+          <CarouselComp className={"services"}>
+            <div className="service-item">
+              <Wd2 />
+              <p className="service-title">Energy Makers Academy</p>
+              <p className="wd-down-arrow">↓</p>
+              <p className="service-sub">
+                We teach students through a “learn by making” approach,
+                providing them with the skills and knowledge to develop
+                universal access to electricity.
+              </p>
+              <a href="#">
+                <p className="linkText">Find out more</p>
+              </a>
+            </div>
+            <div className="service-item">
+              <Mch />
+              <p className="service-title">Energy Development Toolkits</p>
+              <p className="wd-down-arrow">↓</p>
+              <p className="service-sub">
+                Our mobile learning platform and integrated hardware demystifies
+                energy systems, and offers powerful capabilities for their
+                design.
+              </p>
+              <a href="#">
+                <p className="linkText">Find out more</p>
+              </a>
+            </div>
+
+            <div lg={12} sm className="service-item">
+              <Pip />
+              <p className="service-title">Reduce Energy Poverty</p>
+              <p className="wd-down-arrow">↓</p>
+              <p className="service-sub">
+                We empower and inspire people and communities to innovate
+                towards affordable, reliable and sustainable energy systems.
+              </p>
+              <a href="#">
+                <p className="linkText">Find out more</p>
+              </a>
+            </div>
+          </CarouselComp>
+        </div>
       </div>
     </section>
   );
